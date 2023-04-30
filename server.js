1a89467bd822cc81382969f2a153661b2e943fd6
shin101 <shinj0127@gmail.com>
2023-04-29 17:18:42 -0700

    prisma setup complete

---
+++ server.js
@@ -0,0 +1,35 @@
+import {ApolloServer, gql} from "apollo-server";
+
+// graphQL schema
+const typeDefs = gql`
+  type Query {
+    hello:String
+  }
+  type Mutation {
+    createMovie(title:String!): Boolean
+    deleteMovie(title:String!): Boolean
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+  Mutation: {
+    createMovie: (_, args) => {
+      console.log(args);
+      return true
+    },
+    deleteMovie: (_, args) => {
+      console.log(args);
+      return true
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(() => console.log("Sever is running on http://localhost:4000/"));
