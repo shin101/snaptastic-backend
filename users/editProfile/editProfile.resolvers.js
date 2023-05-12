1c9af13f936576b4f5130215c6a9ea597159af31
shin101 <shinj0127@gmail.com>
2023-05-11 17:12:21 -0700

    updated updateProfile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,31 +1,34 @@
-
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import client from "../../client";
 
 
 export default {
   Mutation: {
-    login: async (_, { username, password }) => {
-      const user = await client.user.findFirst({ where: { username } });
-      if (!user) {
+    editProfile: async (_, {
+      firstName, lastName, username, email, password:newPassword }) => {
+        let hashedPw = null;
+        if(newPassword){
+          hashedPw = await bcrypt.hash(newPassword, 10);
+        }
+        const updatedUser = await client.user.update({
+          where:{
+            id:1
+          }, 
+          data:{
+            firstName, lastName, username, email, ...(hashedPw && {password: hashedPw}),
+          },
+      });
+
+      if(updatedUser.id){
         return {
-          ok: false,
-          error: "User not found.",
-        };
-      }
-      const passwordOk = await bcrypt.compare(password, user.password);
-      if (!passwordOk) {
+          ok:true
+        }
+      } else {
         return {
-          ok: false,
-          error: "Incorrect password.",
-        };
+          ok:false,
+          error:"Could not update profile",
+        }
       }
-      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
-      return {
-        ok: true,
-        token,
-      };
     },
   },
 };
