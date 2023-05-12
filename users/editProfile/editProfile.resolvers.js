e270ce5eb0fc8f1a839b690901804e00e1a49ddc
shin101 <shinj0127@gmail.com>
2023-05-12 13:58:11 -0700

    authentication part 1

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,18 +1,20 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import client from "../../client";
 
 
 export default {
   Mutation: {
     editProfile: async (_, {
-      firstName, lastName, username, email, password:newPassword }) => {
+      firstName, lastName, username, email, password:newPassword, token }) => {
+        const { id } = await jwt.verify(token, process.env.SECRET_KEY)
         let hashedPw = null;
         if(newPassword){
           hashedPw = await bcrypt.hash(newPassword, 10);
         }
         const updatedUser = await client.user.update({
           where:{
-            id:1
+            id
           }, 
           data:{
             firstName, lastName, username, email, ...(hashedPw && {password: hashedPw}),
