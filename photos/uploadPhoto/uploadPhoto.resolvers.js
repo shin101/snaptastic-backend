b31462110ab68ef0a4292cc045b15d124f1acc01
shin101 <shinj0127@gmail.com>
2023-06-17 21:56:39 +0900

    Upload photo done

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,10 +1,32 @@
+import client from "../../client";
+import { protectedResolver } from "../../users/users.utils";
+
 export default {
   Mutation: {
     uploadPhoto: protectedResolver(
       async (_, { file, caption }, { loggedInUser }) => {
+        let hashtagObj = [];
         if (caption) {
-          return "hi";
+          const hashtags = caption.match(/#[\w]+/g);
+          hashtagObj = hashtags.map((hashtag) => ({
+            where: { hashtag },
+            create: { hashtag },
+          }));
         }
+        return client.photo.create({
+          data: {
+            file,
+            caption,
+            user: {
+              connect: {
+                id: loggedInUser.id,
+              },
+            },
+            ...(hashtagObj.length > 0 && {
+              hashtags: { connectOrCreate: hashtagObj },
+            }),
+          },
+        });
       }
     ),
   },
