7764c4d39acbca49e73b2c1effb3dd369c5fc407
shin101 <shinj0127@gmail.com>
2023-06-25 23:05:03 +0900

    protected resolver refactor

--- src/users/users.utils.js
+++ src/users/users.utils.js
@@ -1,15 +1,14 @@
 import jwt from "jsonwebtoken";
 import client from "../client";
 
-export const getUser = async(token) => {
+export const getUser = async (token) => {
   try {
-    if(!token){
+    if (!token) {
       return null;
     }
     const { id } = await jwt.verify(token, process.env.SECRET_KEY);
-    const user = await client.user.findUnique(
-      { where: { id }});
-    if(user){
+    const user = await client.user.findUnique({ where: { id } });
+    if (user) {
       return user;
     } else {
       return null;
@@ -19,16 +18,21 @@ export const getUser = async(token) => {
   }
 };
 
-
-
-export function protectedResolver(ourResolver){
-  return function(root, args, context, info){
-    if(!context.loggedInUser){
-      return {
-        ok: false,
-        error: "Please log in to perform this action"
-      };
+export function protectedResolver(ourResolver) {
+  return function (root, args, context, info) {
+    // info tells you if you are trying a query or mutation operation
+    if (!context.loggedInUser) {
+      const query = info.operation.operation === "query";
+      if (query) {
+        return null;
+      } else {
+        // if it is a mutation
+        return {
+          ok: false,
+          error: "Please log in to perform this action",
+        };
+      }
     }
-    return ourResolver(root, args, context, info)
-  }
+    return ourResolver(root, args, context, info);
+  };
 }
