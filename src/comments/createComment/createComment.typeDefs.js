870405694c998f13e290e74092351a78df78cb04
shin101 <kendras0127@gmail.com>
2023-06-29 20:08:02 +0900

    test

--- src/comments/createComment/createComment.typeDefs.js
+++ src/comments/createComment/createComment.typeDefs.js
@@ -1,7 +1,11 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
+  type createCommentResult {
+    ok: Boolean!
+    error: String
+  }
   type Mutation {
-    createComment(photoId: Int!, payload: String!): MutationResponse!
+    createComment(photoId: Int!, payload: String!): createCommentResult!
   }
 `;
