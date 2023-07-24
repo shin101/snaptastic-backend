93755ad8d9fb7d7719fbb3adee2fb5e6c0eba771
shin101 <kendras0127@gmail.com>
2023-07-24 17:07:53 +0900

    create comment udpated

--- src/comments/createComment/createComment.typeDefs.js
+++ src/comments/createComment/createComment.typeDefs.js
@@ -3,10 +3,10 @@ import { gql } from "apollo-server-express";
 export default gql`
   type createCommentResult {
     ok: Boolean!
+    id: Int
     error: String
   }
   type Mutation {
     createComment(photoId: Int!, payload: String!): createCommentResult!
   }
 `;
-
